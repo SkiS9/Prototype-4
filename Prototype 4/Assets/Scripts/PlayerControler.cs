b20c1b3dfb08151438af5a321239c3bb9f710948
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-11-09 14:26:17 -0800

    Left off at 4.2

--- Prototype 4/Assets/Scripts/PlayerControler.cs
+++ Prototype 4/Assets/Scripts/PlayerControler.cs
@@ -19,7 +19,7 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float forwardInput = Input.GetAxis("Verticle");
+        float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
