c6769385e65a495061a68be8d3854eec3ee57832
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-11-28 14:23:49 -0800

    Left off at 4.?

--- Prototype 4/Assets/Scripts/Enemy.cs
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -13,12 +13,13 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb= GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized * speed;
     }
 }
