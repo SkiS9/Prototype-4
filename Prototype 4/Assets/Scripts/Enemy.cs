e10acfdd84377634cdc053f8e93a0a276c76654b
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-12-04 14:16:57 -0800

    In tha middle of 4.3.6

--- Prototype 4/Assets/Scripts/Enemy.cs
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -20,6 +20,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDirection = (player.transform.position - transform.position).normalized * speed;
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
     }
 }
